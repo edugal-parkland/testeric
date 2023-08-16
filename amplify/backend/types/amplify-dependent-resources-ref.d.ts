3a1085f303d39ad40a243740bb56ce0a423bcd07
edugal-parkland <eric.dugal@parkland.ca>
2023-08-16 13:23:36 -0400

    Added graphql and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "testericbucket": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
